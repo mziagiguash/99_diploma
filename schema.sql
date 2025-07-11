360750909237319faabd1e68bcdfc1bd6c8b5afd
Mzia GIGUASHVILI <giguashvilimzia@gmail.com>
2025-07-11 08:05:43 +0400

    Add session table and update SQL schema with trigger for title_search

--- schema.sql
+++ schema.sql
@@ -19,6 +19,17 @@ CREATE TABLE IF NOT EXISTS notes (
   title_search TEXT
 );
 
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid");
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
 -- Обновляем существующие записи для title_search
 UPDATE notes SET title_search = unaccent(lower(title));
 
