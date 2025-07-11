d4dfe0918c980a1c9081d144e24f625ec32d3f71
Mzia GIGUASHVILI <giguashvilimzia@gmail.com>
2025-07-11 07:03:10 +0400

    Add SSL support for Render PostgreSQL

--- db/database.js
+++ db/database.js
@@ -1,9 +1,11 @@
 const { Pool } = require("pg");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // важно для Render PostgreSQL
+  },
 });
 
 module.exports = {
